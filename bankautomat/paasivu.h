539ea94d0b4424c9ffb3cb330d4a1241c30a5cb0
Mokko <Laukkanen.mikko@gmail.com>
2021-11-24 13:30:17 +0200

    readme paivitetty ja app.js paivitetty

--- bankautomat/paasivu.h
+++ bankautomat/paasivu.h
@@ -2,8 +2,9 @@
 #define PAASIVU_H
 
 #include <QDialog>
-
+#include <qnetworkaccessmanager.h>
 #include <QTimer>
+#include <QDebug>
 
 namespace Ui {
 class paaSivu;
@@ -21,11 +22,14 @@ private slots:
     void on_kirjauduNappi_clicked();
     void on_takaisinNappi_clicked();
     void myTimerSlot();
+    void loginSlot (QNetworkReply *reply);
 
 private:
     Ui::paaSivu *ui;
     short timerCounter;
     QTimer *olioQtimer;
+    QNetworkAccessManager *loginManager;            //lis'tty
+    QNetworkAccessManager *reply;                   //lis'tty
 
 signals:
 
